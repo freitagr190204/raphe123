9d2ebae34c524ef48ae736971db13b91032dd739
Raphael Freitag <freitagr190204@sus.htl-grieskirchen.at>
2022-09-28 23:00:51 +0200

    Zahl aus Konsole einlesen

--- Hausuebung1_Erastohenes/src/Main.java
+++ Hausuebung1_Erastohenes/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in, "Windows-1252");
         System.out.println("Wieviele Primzahlen wollen Sie?");
+        int i = sc.nextInt();
         
     }
 }
