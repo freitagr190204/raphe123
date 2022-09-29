973f6e8c14af2d3e5cebd1be197ed2a3a0416457
Raphael Freitag <freitagr190204@sus.htl-grieskirchen.at>
2022-09-29 09:30:05 +0200

    Klasse NumberTester erstellt + erste Methoden

--- Hausuebung2/src/HelloJavamitForEach.java
+++ Hausuebung2/src/HelloJavamitForEach.java
@@ -1,2 +1,15 @@
+import java.util.LinkedList;
+
 public class HelloJavamitForEach {
+    private LinkedList<String> list;
+
+    public HelloJavamitForEach(LinkedList<String> list) {
+        list = new LinkedList<>();
+        list.add("simon");
+        list.add("raphe");
+    }
+
+    public void Ausgabe(){
+        list.forEach(System.out :: print);
+    }
 }
