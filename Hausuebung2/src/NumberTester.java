a8162e16ead6da933109ad3420c534f953f341ed
Raphael Freitag <freitagr190204@sus.htl-grieskirchen.at>
2022-09-29 09:33:12 +0200

    Interface hinzugefügt

--- Hausuebung2/src/NumberTester.java
+++ Hausuebung2/src/NumberTester.java
@@ -1,3 +1,22 @@
 public class NumberTester {
-    
+
+    public NumberTester(String fileName){
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester){
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile(){
+
+    }
 }
